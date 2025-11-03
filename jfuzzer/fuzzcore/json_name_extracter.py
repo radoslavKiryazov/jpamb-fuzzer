4e7ca7cac3c425f8613553db5947b6383913eabb
Pikike <patayviki@gmail.com>
2025-11-03 16:26:55 +0100

    Update json_name_extracter.py

--- jfuzzer/fuzzcore/json_name_extracter.py
+++ jfuzzer/fuzzcore/json_name_extracter.py
@@ -1,13 +1,13 @@
 import fuzzer as fuzzer
-class methodidextracter:
+class json_name_extracter:
     "Extract the method id from the classes"
 
     def __init__(self) -> None:
         pass
 
-    def extracrt_json(method_id):
+    def extract_json(method_id):
         #extract the name of the json from the given file
         line=fuzzer.codefinder(method_id)
         json_name_list=line.split()
         json_name=json_name_list[2]
-        return json_name
+        return json_name+".json"
